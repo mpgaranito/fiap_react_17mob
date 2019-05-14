3ed8a789e3f237f407804fc80e7508806da1f6ce
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-05-13 23:02:41 -0300

    Search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import logo from './logo.svg';
+
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import Header from './components/Header/index.jsx'
+import Header from './components/Header';
 import Routes from './routes';
+
 class App extends Component {
   render() {
-   return (
-   <div className="App">
-    <Header/>
-      <header className="App-header">
-       
-       
-   
-      </header>
-    </div>
-   )
+    return ( <div className = "mdl-layout" >
+      <Router>
+      <Header/>
+      <Routes/>
+      </Router> 
+
+      </div>
+    );
   }
 }
 
