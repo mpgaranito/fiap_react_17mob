689b205a0e5a82748076c4b9f3696174475ac001
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-05-20 22:58:21 -0300

    routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Search from './pages/Search/' ;
-import Product from './pages/Product/' ;
+import Product from './pages/Product/';
+const path = window.location.hostname !== 'localhost' ? '/fiap_react_17mob/' : '/';
 const Routes = () => (
     <Switch>
         <Route
             exact
-            path='/'
+            path={path}
             component={Search}
         />
        
         <Route exact
-            path='/Product/:id'
+            path={`${path}Product/:i`}
             component={Product}
         />
         <Route
