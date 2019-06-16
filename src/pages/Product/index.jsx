56e1de012eb1df9eba844471803181d23e1f973d
Marcos Garanito <mpgaranito@gmail.com>
2019-06-16 12:36:11 -0300

    Correções Básicas de formatação

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,73 +1,87 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios';
-import NumberFormat from 'react-number-format';
-import formatPrice from '../../utils/format'
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import NumberFormat from "react-number-format";
+import formatPrice from "../../utils/format";
 
 class Product extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      id: props.match.params.id,
+      data: {}
+    };
+  }
 
-        this.state = {
-            id: props.match.params.id,
-            data: {},
-        };
-
-    }
-
-    componentDidMount() {
-
-        axios.all([
-            axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
-            axios.get(`https://api.mercadolibre.com/items/${this.state.id}/description`)
-        ]).then(([item, description]) => {
-            console.log(item, description)
-            this.setState({
-                data: {
-                    ...item.data,
-                    description: description.data.plain_text,
-                }
-            });
-        }).catch((err) => {
-            console.log(err);
+  componentDidMount() {
+    axios
+      .all([
+        axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
+        axios.get(
+          `https://api.mercadolibre.com/items/${this.state.id}/description`
+        )
+      ])
+      .then(([item, description]) => {
+        console.log(item, description);
+        this.setState({
+          data: {
+            ...item.data,
+            description: description.data.plain_text
+          }
         });
-    }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    renderPicture() {
-        const { data } = this.state
-        return (
-            <Fragment>
-                <div className="mdl-grid">
-                        <img src={this.state.data.pictures[0].url} with="100%" alt="Figura Produto" />
-                <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                    Comprar
-                </button> 
-                    <p>{this.state.data.id}</p>
-                    <p>{this.state.data.sold_quantity} Vendedores</p>
-                    <h3>{this.state.data.title}</h3>
-                    {this.state.data.price}
-                    <h3><b><NumberFormat value={formatPrice(this.state.data.price)} displayType={'text'} thousandSeparator={true} prefix={'R$'} /></b></h3>
-               <br/> 
-                 <p>Descrição</p>
-                    <p>{this.state.data.description}</p>
-                </div>
-            </Fragment>
-        );
-    }
+  renderPicture() {
+    const { data } = this.state;
+    return (
+      <Fragment>
+        <div className="mdl-js-layout">
+          <div className="mdl-col-2">
+            {" "}
+            <img
+              src={this.state.data.pictures[0].url}
+              width="300px"
+              alt="Figura Produto"
+            />
+          </div>
+          <div className="mdl-col-2">
+            <p>{this.state.data.sold_quantity} Vendedores</p>
+            <h3>{this.state.data.title}</h3>
+          </div>
+          <div className="mdl-col-2">
+            <h3>
+              <b>{formatPrice(this.state.data.price)}</b>
+            </h3>
+          </div>
+          <div className="mdl-col-2">
+            <h5>
+              <b>Descrição</b>
+            </h5>
+            <p>{this.state.data.description}</p>
+          </div>
+        </div>
+        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+          Comprar
+        </button>
+      </Fragment>
+    );
+  }
 
-    render() {
-        const { data } = this.state;
-        if (!Object.keys(data).length) {
-            return (
-                <div className="mdl-card__title">
-                    <span>Carregando</span>
-                </div>
-            );
-        }
-        return (
-            this.renderPicture()
-        );
+  render() {
+    const { data } = this.state;
+    if (!Object.keys(data).length) {
+      return (
+        <div className="mdl-card__title">
+          <span>Carregando</span>
+        </div>
+      );
     }
+    return this.renderPicture();
+  }
 }
 
 export default Product;
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,14 +1,23 @@
-const formatPrice = (price) => {
-    return new Intl.NumberFormat('pt-BR', { 
-        style: 'currency', 
-        currency: 'BRL',
-        minimumIntegerDigits:2,
-        maximumFractionDigits:4,
-        minimumFractionDigits: 2,
-        minimumSignificantDigits:1,
-        maximumSignificantDigits: 4,
-    }).format(price)
-    
-     
-}
+const formatPrice = price => {
+  var v = price;
+  v = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumIntegerDigits: 1,
+    minimumSignificantDigits: 1,
+    minimumFractionDigits: 2
+  }).format(v);
+  var retorno = "";
+  var arrayOfStrings = v.split(",");
+  if (arrayOfStrings.length === 2) {
+    if (arrayOfStrings[1].length === 1) {
+      arrayOfStrings[1] = arrayOfStrings[1] + "0";
+    }
+    retorno = arrayOfStrings[0] + "," + arrayOfStrings[1];
+  } else {
+    retorno = v + ",00";
+  }
+
+  return retorno;
+};
 export default formatPrice;
